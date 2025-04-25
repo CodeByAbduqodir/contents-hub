b3771f6cca18a84095552c7eef50d5b5dfa0eaab
Mikey <trueptimekhanoid@gmail.com>
2025-04-25 15:57:26 +0500

    PATCH: Some fixes on routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ContentController::class, 'index'])->name('home');
 
 Route::resource('contents', ContentController::class);
